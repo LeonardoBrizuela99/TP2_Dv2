00dd257cd71a764a4a331894eb3cae65a5147ad7
Leo <leo99brizuela@gmail.com>
2026-05-11 05:45:56 -0300

    Add snapping mechanic to align blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -10,6 +10,9 @@ public class Block : MonoBehaviour
     private int _floorLayer;
     private int _blockLayer;
 
+  
+    [SerializeField] private float snapThreshold = 0.2f;
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
@@ -35,12 +38,27 @@ public class Block : MonoBehaviour
     {
         if (_hasLanded) return;
         int otherLayer = collision.gameObject.layer;
+
         if (otherLayer == _floorLayer || otherLayer == _blockLayer)
         {
             _hasLanded = true;
+
           
-            _rb.constraints = RigidbodyConstraints.FreezeRotationY;
-            _rb.mass = 20f;
+            float errorX = Mathf.Abs(transform.position.x - collision.transform.position.x);
+
+            if (errorX < snapThreshold)
+            {
+              
+                transform.position = new Vector3(collision.transform.position.x, transform.position.y, transform.position.z);
+                _rb.constraints = RigidbodyConstraints.FreezeAll;
+            }
+            else
+            {
+                
+                _rb.constraints = RigidbodyConstraints.FreezeRotationY;
+                _rb.mass = 20f;
+            }
+          
 
             if (otherLayer == _blockLayer)
                 GameEvents.TriggerBlockLanded(transform.position.y);
